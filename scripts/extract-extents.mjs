ff67eb814904c79f937b7cec023e118ac421f90a
lane <laneolson@gmail.com>
2022-02-08 14:25:42 -0800

    feat: add 0.01 quantile, 0.99 quantile, median, and avg to extents

--- scripts/extract-extents.mjs
+++ scripts/extract-extents.mjs
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
 import { csvParse, csvFormat, autoType } from "d3-dsv";
-import { extent } from "d3-array";
+import { extent, mean, median, quantile } from "d3-array";
 
 /**
  * This script re-formats the scorecard data from long to wide format
@@ -16,8 +16,17 @@ const extents = data.columns.reduce((result, col) => {
   if (col.endsWith("_r")) return result;
   if (col.endsWith("_i")) return result;
   if (col.endsWith("_p")) return result;
+  if (col === "GEOID") return result;
   const extentValue = extent(data, (row) => row[col]);
-  result[col] = { id: col, min: extentValue[0], max: extentValue[1] };
+  result[col] = {
+    id: col,
+    min: extentValue[0],
+    max: extentValue[1],
+    q1: quantile(data, 0.01, (row) => row[col]),
+    q99: quantile(data, 0.99, (row) => row[col]),
+    median: median(data, (row) => row[col]),
+    avg: mean(data, (row) => row[col]),
+  };
   return result;
 }, {});
 
@@ -27,7 +36,7 @@ const wideCsv = csvFormat(
     if (a.id > b.id) return 1;
     return 0;
   }),
-  ["id", "min", "max"]
+  ["id", "min", "max", "q1", "q99", "median", "avg"]
 );
 if (output) writeFileSync(output, wideCsv);
 if (!output) process.stdout.write(wideCsv);
